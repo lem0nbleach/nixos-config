79f9a5377a6e304627c84a53bd55d77031be6cb8
Lem0nbleach <feiyang.wu@fishstall.net>
2024-08-16 22:22:09 -0400

    added lualine & neotree for nixvim

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -3,4 +3,13 @@
 {
   programs.nixvim.enable = true;
 
+  programs.nixvim = {
+
+    defaultEditor = true;
+
+    plugins = {
+      lualine.enable = true;
+      neo-tree.enable = true;
+    };
+  };
 }

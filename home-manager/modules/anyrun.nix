f1db65bf3721273273740f61436f6df92745c388
Lem0nbleach <feiyang.wu@fishstall.net>
2024-08-18 13:00:44 -0400

    added anyrun

--- home-manager/modules/anyrun.nix
+++ home-manager/modules/anyrun.nix
@@ -1,7 +1,25 @@
-{ ... }:
+{ pkgs, inputs, ... }:
 
 {
   programs.anyrun = {
     enable = true;
+    config = {
+      plugins = [
+         inputs.anyrun.packages.${pkgs.system}.applications
+	 inputs.anyrun.packages.${pkgs.system}.shell
+	 inputs.anyrun.packages.${pkgs.system}.websearch
+      ];
+      x = { fraction = 0.5; };
+      y = { fraction = 0.3; };
+      width = { fraction = 0.3; };
+      hideIcons = false;
+      layer = "overlay";
+      ignoreExclusiveZones = false;
+    };
+    extraCss = ''
+      window {
+        background-color: transparent;
+      }
+    '';
   };
 }

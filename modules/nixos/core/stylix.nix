24918860fcb48f044cf1c8d40a6c253f369ff6fa
Lem0nbleach <feiyang.wu@fishstall.net>
2025-01-12 16:17:26 -0500

    fix wallpaper path on stylix and hyprpaper

--- modules/nixos/core/stylix.nix
+++ modules/nixos/core/stylix.nix
@@ -22,7 +22,7 @@
     base0F = "FF61AF"; #FF61AF
   };
   stylix.polarity = "dark";
-  stylix.image = ./home-manager/window-manager/wallpaper/EYE.jpg;
+  stylix.image = ../../home-manager/window-manager/wallpaper/EYE.jpg;
   stylix.cursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";

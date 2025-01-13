6c44425831b2ea287b15202300c7318d88723f4d
Lem0nbleach <feiyang.wu@fishstall.net>
2025-01-13 18:45:58 -0500

    remove package section in configuration.nix

--- hosts/docker-host/configuration.nix
+++ hosts/docker-host/configuration.nix
@@ -54,13 +54,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

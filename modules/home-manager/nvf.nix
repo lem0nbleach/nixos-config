1ebd347c772094ced5e5585e12b14016177e8596
Lem0nbleach <feiyang.wu@fishstall.net>
2025-01-19 15:15:51 -0500

    import stylix theme to nvf and adjust plugins

--- modules/home-manager/nvf.nix
+++ modules/home-manager/nvf.nix
@@ -1,14 +1,22 @@
+{ config, ... }:
+
 {
   programs.nvf = {
     enable = true;
     settings = {
       vim = {
       
-        languages.nix = {
-	  enable = true;
-	  format.enable = true;
-	  lsp.enable = true;
+        languages = {
+
+          enableFormat = true;
+          enableLSP = true;
+          enableTreesitter = true;
+
+          nix.enable = true;
+
 	};
+
+        autocomplete.nvim-cmp.enable = true;
 
         filetree = {
           neo-tree = {
@@ -29,6 +37,38 @@
           lualine = {
             enable = true;
           };
+        };
+
+        visuals = {
+          nvim-scrollbar.enable = true;
+        };
+
+        theme = {
+          enable = true;
+          name = "base16";
+
+          base16-colors = 
+            let
+              inherit (config.lib.stylix) colors;
+            in
+            {
+            base00 = colors.base00;
+            base01 = colors.base01;
+            base02 = colors.base02;
+            base03 = colors.base03;
+            base04 = colors.base04;
+            base05 = colors.base05;
+            base06 = colors.base06;                                     
+            base07 = colors.base07;
+            base08 = colors.base08;
+            base09 = colors.base09;
+            base0A = colors.base0A;
+            base0B = colors.base0B;
+            base0C = colors.base0C;
+            base0D = colors.base0D;
+            base0E = colors.base0E;
+            base0F = colors.base0F;
+            };
         };
       };
     };

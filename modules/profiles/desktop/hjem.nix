33ff5a0d09e10631961ae2315085ca000e706455
Feiyang Wu <feiyang.wu@fishstall.net>
2025-04-23 17:21:12 -0400

    hjem: added rofi config

--- modules/profiles/desktop/hjem.nix
+++ modules/profiles/desktop/hjem.nix
@@ -1,7 +1,8 @@
 {
   hjem.users = {
     lem0nbleach.files = {
-      ".config/MangoHud/MangoHud.conf".source = ../../config/MangoHud.conf;
+      ".config/MangoHud/MangoHud.conf".source = ../../config/MangoHud/MangoHud.conf;
+      ".config/rofi/config.rasi".source = ../../config/rofi/config.rasi;
     };
   };
 }

72c8fee0e6ca945a95b3d8350adbf712566e0451
Feiyang Wu <feiyang.wu@fishstall.net>
2025-06-06 11:06:52 -0400

    fish: update the greeting the original is too long and not useful anymore

--- modules/config/fish/config.fish
+++ modules/config/fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    random choice "Hello mate!" "Hi there!" "G'day!" "Howdy!"
+end
+
 zoxide init fish | source
 thefuck --alias | source

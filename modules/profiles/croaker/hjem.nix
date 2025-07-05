0bd1a722a47012b0db8c2a107927ae1dd20c9ace
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-05 13:11:49 -0400

    Croaker/hjem: refactor

--- modules/profiles/croaker/hjem.nix
+++ modules/profiles/croaker/hjem.nix
@@ -1,66 +1,24 @@
+{ lib, ... }:
+
 {
-  hjem.users = {
-    lem0nbleach.files = {
-      ".config/MangoHud/MangoHud.conf" = {
-        source = ../../config/MangoHud/MangoHud.conf;
-        clobber = true;
-      };
-      ".config/rofi/config.rasi" = {
-        source = ../../config/rofi/config.rasi;
-        clobber = true;
-      };
-      ".config/hypr/hyprland.conf" = {
-        source = ../../config/hypr/hyprland.conf;
-        clobber = true;
-      };
-      ".config/hypr/hypridle.conf" = {
-        source = ../../config/hypr/hypridle.conf;
-        clobber = true;
-      };
-      ".config/hypr/hyprlock.conf" = {
-        source = ../../config/hypr/hyprlock.conf;
-        clobber = true;
-      };
-      ".config/hypr/hyprpaper.conf" = {
-        source = ../../config/hypr/hyprpaper.conf;
-        clobber = true;
-      };
-      ".zshrc" = {
-        source = ../../config/zsh/zshrc;
-        clobber = true;
-      };
-      ".config/foot/foot.ini" = {
-        source = ../../config/foot/foot.ini;
-        clobber = true;
-      };
-      ".mozilla/firefox/user.js" = {
-        source = ../../config/mozilla/firefox/user.js;
-        clobber = true;
-      };
-      ".config/gtk-3.0/settings.ini" = {
-        source = ../../config/gtk-3.0/desktop.settings.ini;
-        clobber = true;
-      };
-      ".gtkrc-2.0" = {
-        source = ../../config/gtkrc-2.0/desktop-gtkrc-2.0;
-        clobber = true;
-      };
-      "./local/share/icons/default/index.theme" = {
-        source = ../../config/icons/default/desktop-index.theme;
-        clobber = true;
-      };
-      ".config/xsettingsd/xsettingsd.conf" = {
-        source = ../../config/xsettingsd/xsettingsd.conf;
-        clobber = true;
-      };
-      ".config/helix/config.toml" = {
-        source = ../../config/helix/config.toml;
-        clobber = true;
-      };
-      ".config/fish/config.fish" = {
-        source = ../../config/fish/config.fish;
-        clobber = true;
-      };
+  hjem.users.lem0nbleach = {
+    clobberFiles = lib.mkForce true;
+    files = {
+      ".config/MangoHud/MangoHud.conf".source = ../../config/MangoHud/MangoHud.conf;
+      ".config/rofi/config.rasi".source = ../../config/rofi/config.rasi;
+      ".config/hypr/hyprland.conf".source = ../../config/hypr/hyprland.conf;
+      ".config/hypr/hypridle.conf".source = ../../config/hypr/hypridle.conf;
+      ".config/hypr/hyprlock.conf".source = ../../config/hypr/hyprlock.conf;
+      ".config/hypr/hyprpaper.conf".source = ../../config/hypr/hyprpaper.conf;
+      ".zshrc".source = ../../config/zsh/zshrc;
+      ".config/foot/foot.ini".source = ../../config/foot/foot.ini;
+      ".mozilla/firefox/user.js".source = ../../config/mozilla/firefox/user.js;
+      ".config/gtk-3.0/settings.ini".source = ../../config/gtk-3.0/desktop.settings.ini;
+      ".gtkrc-2.0".source = ../../config/gtkrc-2.0/desktop-gtkrc-2.0;
+      "./local/share/icons/default/index.theme".source = ../../config/icons/default/desktop-index.theme;
+      ".config/xsettingsd/xsettingsd.conf".source = ../../config/xsettingsd/xsettingsd.conf;
+      ".config/helix/config.toml".source = ../../config/helix/config.toml;
+      ".config/fish/config.fish".source = ../../config/fish/config.fish;
     };
   };
 }

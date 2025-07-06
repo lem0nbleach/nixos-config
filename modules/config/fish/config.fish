a7635259486067bd84887ac05cb2046451494eec
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-06 12:15:18 -0400

    hydro: colored the prompt symbol to blue easier to identify

--- modules/config/fish/config.fish
+++ modules/config/fish/config.fish
@@ -1,9 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 function fish_greeting
     random choice "Hello mate!" "Hi there!" "G'day!" "Howdy!"
 end
+
+set -U hydro_color_prompt blue
 
 zoxide init fish | source

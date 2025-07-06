a0b13ed66085fa4acaada331fde61999d9dae1b8
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-06 12:18:30 -0400

    anchovy/hjem: refactor

--- modules/profiles/anchovy/hjem.nix
+++ modules/profiles/anchovy/hjem.nix
@@ -1,62 +1,23 @@
+{ lib, ... }:
+
 {
-  hjem.users = {
-    lem0nbleach.files = {
-      ".config/hypr/hyprland.conf" = {
-        source = ../../config/hypr/anchovy-hyprland.conf;
-        clobber = true;
-      };
-      ".config/hypr/hypridle.conf" = {
-        source = ../../config/hypr/hypridle.conf;
-        clobber = true;
-      };
-      ".config/hypr/hyprlock.conf" = {
-        source = ../../config/hypr/hyprlock.conf;
-        clobber = true;
-      };
-      ".config/hypr/hyprpaper.conf" = {
-	      source = ../../config/hypr/hyprpaper.conf;
-	      clobber = true;
-      };
-      ".mozilla/firefox/nh8kebxc.default/user.js" = {
-        source = ../../config/mozilla/firefox/user.js;
-        clobber = true;
-      };
-      ".config/foot/foot.ini" = {
-	      source = ../../config/foot/foot.ini;
-	      clobber = true;
-      };
-      ".config/fish/config.fish" = {
-	      source = ../../config/fish/config.fish;
-	      clobber = true;
-      };
-      ".config/helix/config.toml" = {
-        source = ../../config/helix/config.toml;
-        clobber = true;
-      };
-      ".config/gtk-4.0/settings.ini" = {
-        source = ../../config/gtk-4.0/settings.ini;
-        clobber = true;
-      };
-      ".config/gtk-3.0/settings.ini" = {
-        source = ../../config/gtk-3.0/settings.ini;
-        clobber = true;
-      };
-      ".gtkrc-2.0" = {
-        source = ../../config/gtkrc-2.0/gtkrc-2.0;
-        clobber = true;
-      };
-      ".icons/default/index.theme" = {
-        source = ../../config/icons/default/index.theme;
-        clobber = true;
-      };
-      ".config/xsettingsd/xsettingsd.conf" = {
-        source = ../../config/xsettingsd/xsettingsd.conf;
-        clobber = true;
-      };
-      ".config/watt.toml" = {
-        source = ../../config/watt/watt.toml;
-        clobber = true;
-      };
+  hjem.users.lem0nbleach = {
+    clobberFiles = lib.mkForce true;
+    files = {
+      ".config/hypr/hyprland.conf".source = ../../config/hypr/anchovy-hyprland.conf;
+      ".config/hypr/hypridle.conf".source = ../../config/hypr/hypridle.conf;
+      ".config/hypr/hyprlock.conf".source = ../../config/hypr/hyprlock.conf;
+      ".config/hypr/hyprpaper.conf".source = ../../config/hypr/hyprpaper.conf;
+      ".mozilla/firefox/nh8kebxc.default/user.js".source = ../../config/mozilla/firefox/user.js;
+      ".config/foot/foot.ini".source = ../../config/foot/foot.ini;
+      ".config/fish/config.fish".source = ../../config/fish/config.fish;
+      ".config/helix/config.toml".source = ../../config/helix/config.toml;
+      ".config/gtk-4.0/settings.ini".source = ../../config/gtk-4.0/settings.ini;
+      ".config/gtk-3.0/settings.ini".source = ../../config/gtk-3.0/settings.ini;
+      ".gtkrc-2.0".source = ../../config/gtkrc-2.0/gtkrc-2.0;
+      ".icons/default/index.theme".source = ../../config/icons/default/index.theme;
+      ".config/xsettingsd/xsettingsd.conf".source = ../../config/xsettingsd/xsettingsd.conf;
+      ".config/watt.toml".source = ../../config/watt/watt.toml;
     };
   };
 }

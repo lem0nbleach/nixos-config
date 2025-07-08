95dad86b098056ef26e04b0947f56137e9aebabd
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-08 19:16:48 -0400

    Anchovy/fish/zellij: update start up script The new ones allows fast exit

--- config/dots/fish/config.fish
+++ config/dots/fish/config.fish
@@ -2,9 +2,11 @@ function fish_greeting
     random choice "Hello mate!" "Hi there!" "G'day!" "Howdy!"
 end
 
-if set -q ZELLIJ
-else
-    zellij
+if status is-interactive
+    # Configure auto-attach/exit to your likings (default is off).
+    # set ZELLIJ_AUTO_ATTACH true
+    set ZELLIJ_AUTO_EXIT true
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 set -U hydro_color_prompt blue

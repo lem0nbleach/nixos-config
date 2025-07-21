935bbb7ddeee074818ee9ae40ee4a04c251f8d5f
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-21 14:09:16 -0400

    marlin/tailscale: added tailscale for marlin

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,6 +1,6 @@
 { lib, config, ... }:
 
-lib.mkIf config.anchovy {
+lib.mkIf (config.anchovy || config.marlin) {
   services.tailscale.enable = true;
   # needed in order to use an exit node
   networking.firewall.checkReversePath = "loose";

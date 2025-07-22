b8751b6e98238b492a93dc787642c1ee5fa8ddb4
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-22 19:13:37 -0400

    hyprsunset: fixed major logic bug

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,5 +1,30 @@
 { lib, config, pkgs, inputs, ... }:
 
+let
+  hyprsunsetScript = pkgs.writeShellApplication {
+    name = "sunset-checker";
+    runtimeInputs = [ pkgs.hyprland pkgs.procps ];
+    text = ''
+      # Get current time in minutes
+      current_hour=$(date +%H)
+      current_minute=$(date +%M)
+      current_time_minutes=$((current_hour * 60 + current_minute))
+
+      # Convert times to minutes by Hours*60 + Minutes
+      evening_start=1200  # 8:00 PM
+      morning_end=270     # 4:30 AM
+
+      if [[ "$current_time_minutes" -ge "$evening_start" ]] || [[ "$current_time_minutes" -le "$morning_end" ]]; then
+        echo "Changing to nighttime hyprsunset"
+        hyprctl hyprsunset temperature 4500
+      else
+        echo "Changing to default hyprsunset"
+        hyprctl hyprsunset identity
+      fi
+    '';
+  };
+
+in
 lib.mkMerge [
   (lib.mkIf (config.anchovy || config.croaker) {
     programs.hyprland = {
@@ -37,20 +62,17 @@ lib.mkMerge [
       description = "Run hyprsunset check";
       serviceConfig = {
         Type = "oneshot";
-        ExecStart ="${pkgs.bash}/bin/bash /home/lem0nbleach/nixos/modules/scripts/auto-sunset.sh";
+        ExecStart = lib.getExe hyprsunsetScript;
       };
-      path = [
-        pkgs.hyprsunset
-        pkgs.procps
-      ];
     };
     systemd.user.timers.hyprsunset = {
       description = "Run hyprsunset check every minute";
-      wantedBy = [ "timers.target" ];
+      wantedBy = [ "graphical-session.target" ];
       after = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
       timerConfig = {
+        AccuracySec = "1s";
         OnCalendar = "*:0/1";  # Every minute
-        Persistent = true;
       };
     };
   })

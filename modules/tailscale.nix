75e24628ee5a0216e8064b08bf510b207f4570c8
Feiyang Wu <feiyang.wu@fishstall.net>
2025-07-24 18:24:43 -0400

    marlin/tailscale: moved routing feature setting to the module

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,13 +1,13 @@
 { lib, config, ... }:
 
 lib.mkMerge [
-  lib.mkIf (config.anchovy || config.marlin) {
+  (lib.mkIf (config.anchovy || config.marlin) {
     services.tailscale.enable = true;
     # needed in order to use an exit node
     networking.firewall.checkReversePath = "loose";
-  }
+  })
 
-  lib.mkIf config.marlin {
+  (lib.mkIf config.marlin {
     services.tailscale.useRoutingFeatures = "both";
-  }
+  })
 ]

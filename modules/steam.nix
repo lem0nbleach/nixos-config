911496dc5f9b33a937a9270993489086441bce87
lem0nbleach <feiyang.wu@fishstall.net>
2025-08-09 20:46:18 -0400

    fix: steam

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,11 +3,31 @@
 lib.mkIf (config.anchovy || config.croaker) {
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true;
+    protontricks.enable = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
   programs.gamemode.enable = true;
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  # Gamescope fix from https://wiki.nixos.org/wiki/Steam#Gamescope_fails_to_launch_when_used_within_Steam
+  programs.steam.package = pkgs.steam.override {
+    extraPkgs = pkgs': with pkgs'; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib # Provides libstdc++.so.6
+      libkrb5
+      keyutils
+      # Add other libraries as needed
+    ];
+  };
 
   environment.systemPackages = [
     pkgs.mangohud

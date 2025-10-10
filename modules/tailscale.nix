912a71101c53ef225d9a44f55ace5acd1bca0d12
lem0nbleach <feiyang.wu@fishstall.net>
2025-10-10 11:29:18 -0400

    tailscale update

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 lib.mkMerge [
   (lib.mkIf (config.anchovy || config.marlin) {
@@ -9,5 +9,20 @@ lib.mkMerge [
 
   (lib.mkIf config.marlin {
     services.tailscale.useRoutingFeatures = "both";
+    environment.systemPackages = [
+      pkgs.ethtool
+      pkgs.networkd-dispatcher
+    ];
+    services = {
+      networkd-dispatcher = {
+        enable = true;
+        rules."50-tailscale" = {
+          onState = ["routable"];
+          script = ''
+            "${pkgs.ethtool}/sbin/ethtool" -K ens18 rx-udp-gro-forwarding on rx-gro-list off
+          '';
+        };
+      };
+    };
   })
 ]

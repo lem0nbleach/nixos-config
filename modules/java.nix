b7c5f49865eec8fd4940d1bf94dba8f0530555f9
Feiyang Wu <feiyang.wu@fishstall.net>
2026-04-19 22:08:19 -0400

    added javafx to croaker

--- modules/java.nix
+++ modules/java.nix
@@ -1,6 +1,6 @@
 { lib, config, pkgs, ... }:
 
-lib.mkIf config.anchovy {
+lib.mkIf (config.anchovy || config.croaker) {
   programs.java = {
     enable = true;
     package = pkgs.javaPackages.compiler.openjdk21.override { enableJavaFX = true; };
